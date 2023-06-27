d50e7f2bf60d1a20f9d80ad7cd8b25ddda74d1cf
Samuel Kinyanjui <sax80kinya@gmail.com>
2023-06-27 22:30:13 +0300

    Update script.js
    updated

--- script.js
+++ script.js
@@ -1,13 +1,5 @@
-
-/*Dcoments editted by samuel and shows the use of document.getElementById() and quaryselector
-const p = document.getElementById('message');    message is the id
-console.log(p);
-constant p=document.querySelector ('.mesage')   message is the class
-
-let elements = document.getElementsByName(name);
-
- This java script is being called/invokded as the last itmes in the body so that all the dom elements are declared fiest before calling them*/
- function changeIcon(icon) {
+/* Dcoments editted by samuel  */
+function changeIcon(icon) {
   icon.classList.toggle('bi-x');
 
   const HeaderMenu = document.querySelector('.header-menu');
@@ -24,7 +16,7 @@ changeIcon();
 
 function chanageMenu() {
   const HeaderMenu = document.querySelector('.header-menu');
-  /* used selector querySlector instaeda id getelemntbyid*/
+  /* used selector querySlector instaeda id getelemntbyid */
   /* wel commented   */
   const listIcon = document.querySelector('.bi-list');
   const mainHeader = document.querySelector('.main-header');
